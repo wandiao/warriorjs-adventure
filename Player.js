d865d4218610a799cc4f0ced166c8c739da4aec3
wandiao <851298395@qq.com>
2018-05-26 17:10:54 +0800

    level7

--- Player.js
+++ Player.js
@@ -2,46 +2,32 @@ class Player {
   constructor() {
     this.health = 20;
     this.warrior = null;
-    this.rescued = false;
   }
   playTurn(warrior) {
     if (!this.warrior) { // 保存角色对象
       this.warrior = warrior;
     }
     const health = warrior.health();
-    let space;
-    if (!this.rescued) {
-      space = warrior.feel('backward');
-    } else {
-      space = warrior.feel();
-    }
+    let space = warrior.feel();;
     if (space.isEmpty()) {
-      if (health < 16) { //干死两个弓箭手的最低血量
+      if (health < 10) { //干死弓箭手的最低血量
         if (this.health > health) { // 受到攻击
-          if (health < 6) { // 防止被射死的最低血量
+          if (health < 8) { // 防止被射死的最低血量
             warrior.walk('backward');
           } else {
             warrior.walk();
           }
-          
         } else {
           warrior.rest();
         }
       } else {
-        if (this.rescued) {
-          warrior.walk();
-        } else {
-          warrior.walk('backward');
-        }
-        
+        warrior.walk();
       }
     } else {
-      if (space.getUnit().isEnemy()) { // 攻击敌人
-        warrior.attack();
-      } else if (space.getUnit().isBound()) { // 营救被困的俘虏
-        warrior.rescue('backward');
-        this.rescued = true; // 营救成功
+      if (space.isWall()) {
+        return warrior.pivot();
       }
+      warrior.attack();
     }
     this.health = health;
   }
--- README.md
+++ README.md
@@ -1,27 +1,28 @@
-## caterpillar - beginner - level 6
+## caterpillar - beginner - level 7
 
-### _The wall behind you feels a bit further away in this room. And you hear more cries for help._
+### _You feel a wall right in front of you and an opening behind you._
 
-> **TIP:** You can walk backward by passing `'backward'` as an argument to `walk()`. Same goes for `feel()`, `rescue()` and `attack()`. Archers have a limited attack distance.
+> **TIP:** You're not as effective at attacking backward. Use `warrior.feel().isWall()` and `warrior.pivot()` to turn around.
 
 
 ### Floor Map
 
 ```
-╔════════╗
-║C @ S aa║
-╚════════╝
+╔══════╗
+║>a S @║
+╚══════╝
 
-C = Captive (1 HP)
-@ = caterpillar (20 HP)
-S = Thick Sludge (24 HP)
+> = stairs
 a = Archer (7 HP)
+S = Thick Sludge (24 HP)
+@ = caterpillar (20 HP)
 ```
 
 ### Abilities
 
 #### Actions (only one per turn)
 
+* `warrior.pivot()`: Rotate in the given direction (backward by default).
 * `warrior.walk()`: Move one space in the given direction (forward by default).
 * `warrior.attack()`: Attack a unit in the given direction (forward by default) dealing 5 HP of damage.
 * `warrior.rest()`: Gain 10% of max health back, but do nothing more.
